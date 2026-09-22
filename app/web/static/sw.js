6a4aff96bf109f81722ae5479e2365ae16e49f76
Ajala Abdullah Abiola <webmastersmma@gmail.com>
2026-09-22 20:25:32 +0100

    Landing page: position SafeRoad as an AI agent with a tools section

--- app/web/static/sw.js
+++ app/web/static/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'saferoad-v6';
-const SHELL = ['/static/app.js?v=6', '/static/app.css?v=6', '/static/icons/icon-192.png', '/static/icons/icon-512.png', '/manifest.webmanifest'];
+const CACHE = 'saferoad-v7';
+const SHELL = ['/static/app.js?v=6', '/static/app.css?v=7', '/static/icons/icon-192.png', '/static/icons/icon-512.png', '/manifest.webmanifest'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', (e) => {
